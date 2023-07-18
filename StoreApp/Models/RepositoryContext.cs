c239a7669d406ba21fe69dc1399135be09807588
Eren Gündüzvar <70322002+erengunduzvar@users.noreply.github.com>
2023-07-18 21:20:23 +0300

    Migrations !

--- StoreApp/Models/RepositoryContext.cs
+++ StoreApp/Models/RepositoryContext.cs
@@ -4,6 +4,26 @@ namespace StoreApp.Models
 {
     public class RepositoryContext : DbContext
     {
-        public DbSet<Product> Products { get; set; }   
+        public DbSet<Product> Products { get; set; }
+
+        public RepositoryContext(DbContextOptions<RepositoryContext> options)
+            :base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+                .HasData(
+                new Product() { ProductId = 1, ProductName= "Laptop",Price = 15000},
+                new Product() { ProductId = 2, ProductName = "Computer", Price = 15000 },
+                new Product() { ProductId = 3, ProductName = "Mouse", Price = 698 },
+                new Product() { ProductId = 4, ProductName = "Monitor", Price = 3544 },
+                new Product() { ProductId = 5, ProductName = "Keyboard", Price = 6200 }
+                );
+        }
     }
+
 }
