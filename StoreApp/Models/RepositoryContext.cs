c2fbd78a1ffc0f78e0536e3ffce7e01815075458
Eren Gündüzvar <70322002+erengunduzvar@users.noreply.github.com>
2023-07-18 19:57:57 +0300

    DbSet

---
+++ StoreApp/Models/RepositoryContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StoreApp.Models
+{
+    public class RepositoryContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }   
+    }
+}
