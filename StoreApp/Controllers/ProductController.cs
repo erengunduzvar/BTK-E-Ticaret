f0f21a62b4b677454f00fd086ee0ed5c255f44e6
Eren Gündüzvar <70322002+erengunduzvar@users.noreply.github.com>
2023-07-19 19:39:52 +0300

    Dependency Injection

---
+++ StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        //Dependency Injection
+        private readonly RepositoryContext _context;
+
+        public ProductController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _context.Products.ToList();
+            return View(model);
+        }
+
+        public IActionResult Get(int id)
+        {
+            Product product = _context.Products.First(x => x.ProductId == id);
+            return View(product);
+        }
+
+
+    }
+}
