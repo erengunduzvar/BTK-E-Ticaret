09e546007ead5c9154bbe52d1eebf242422d3cae
Eren Gündüzvar <70322002+erengunduzvar@users.noreply.github.com>
2023-08-11 17:06:11 +0300

    Reposititories & Entities

--- Repositories/RepositoryContext.cs
+++ Repositories/RepositoryContext.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Entities.Models;
 
-namespace StoreApp.Models
+namespace Repositories
 {
     public class RepositoryContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
 
         public RepositoryContext(DbContextOptions<RepositoryContext> options)
-            :base(options)
+            : base(options)
         {
-            
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -17,7 +18,7 @@ namespace StoreApp.Models
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Product>()
                 .HasData(
-                new Product() { ProductId = 1, ProductName= "Laptop",Price = 15000},
+                new Product() { ProductId = 1, ProductName = "Laptop", Price = 15000 },
                 new Product() { ProductId = 2, ProductName = "Computer", Price = 15000 },
                 new Product() { ProductId = 3, ProductName = "Mouse", Price = 698 },
                 new Product() { ProductId = 4, ProductName = "Monitor", Price = 3544 },
@@ -25,5 +26,4 @@ namespace StoreApp.Models
                 );
         }
     }
-
 }
