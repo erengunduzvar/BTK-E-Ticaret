aa3ced92da4a63db7735c77249c8cfbeeb452a81
Eren Gündüzvar <70322002+erengunduzvar@users.noreply.github.com>
2023-08-14 14:48:46 +0300

    Product ve Category Services

---
+++ Services/CategoryManager.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Models;
+using Repositories;
+using Repositories.Contracts;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class CategoryManager : ICategoryService
+    {
+        private IRepositoryManager _repositoryManager;
+
+        public CategoryManager(IRepositoryManager repositoryManager)
+        {
+            _repositoryManager = repositoryManager;
+        }
+
+        public IEnumerable<Category> GetAllCategories(bool trackChanges)
+        {
+            return _repositoryManager.Category.FindAll(trackChanges);
+        }
+    }
+}
---
+++ Services/ProductManager.cs
@@ -0,0 +1,35 @@
+﻿using Entities.Models;
+using Repositories;
+using Repositories.Contracts;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ProductManager : IProductService
+    {
+        IRepositoryManager repositoryManager;
+
+        public ProductManager(IRepositoryManager repositoryManager)
+        {
+            this.repositoryManager = repositoryManager;
+        }
+
+        public IEnumerable<Product> GetAllProducts(bool trackChanges)
+        {
+            return repositoryManager.Product.GetAllProducts(trackChanges);
+        }
+
+        public Product? GetProductById(int id, bool trackChanges)
+        {
+            var product = repositoryManager.Product.GetById(id, trackChanges);
+            if (product is null)
+                throw new Exception("Product is not found");
+            return product;
+        }
+    }
+}
