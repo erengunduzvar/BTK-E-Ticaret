4a15a882f00f8e5db8cfe255a4f2ada0f87ec1c7
Eren Gündüzvar <70322002+erengunduzvar@users.noreply.github.com>
2023-08-14 15:42:35 +0300

    ViewComponent

---
+++ StoreApp/Components/ProductSummary.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repositories;
+using Services.Contracts;
+
+namespace StoreApp.Components
+{
+    public class ProductSummary : ViewComponent
+    {
+        private readonly IServiceManager manager;
+
+        public ProductSummary(IServiceManager manager)
+        {
+            this.manager = manager;
+        }
+
+        public string Invoke()
+        {
+            return manager.ProductService.GetAllProducts(false).Count().ToString();
+        }
+    }
+}
