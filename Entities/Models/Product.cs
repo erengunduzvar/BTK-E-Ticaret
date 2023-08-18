20cd1f6b6d9df99b5e1a9a180bbae4ad261618e2
Eren Gündüzvar <70322002+erengunduzvar@users.noreply.github.com>
2023-08-18 21:22:01 +0300

    Product DTO ve Create Ekranında Bazı değişiklikler

--- Entities/Models/Product.cs
+++ Entities/Models/Product.cs
@@ -6,10 +6,8 @@ namespace Entities.Models
     {
         public int ProductId { get; set; }
 
-        [Required(ErrorMessage = "Ürün İsmi Gerekiyor")]
         public string? ProductName { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Fiyat Gerekiyor")]
         public decimal Price { get; set; }
 
         public int? CategoryId { get; set; } //Foreign Key
