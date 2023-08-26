cc3af5387850506f133146a7c3f8115fd4a68540
Eren Gündüzvar <70322002+erengunduzvar@users.noreply.github.com>
2023-08-26 04:09:46 +0300

    İmgeler ile çalışma

--- StoreApp/Areas/Admin/Controllers/ProductController.cs
+++ StoreApp/Areas/Admin/Controllers/ProductController.cs
@@ -69,10 +69,20 @@ namespace StoreApp.Areas.Admin.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Update([FromForm] ProductDtoForUpdate productDto)
+        public async Task<IActionResult> Update([FromForm] ProductDtoForUpdate productDto, IFormFile file)
         {
             if(ModelState.IsValid)
             {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                productDto.ImageUrl = String.Concat("/images/", file.FileName);
+
+
                 _manager.ProductService.UpdateOneProduct(productDto);
                 return RedirectToAction("Index");
             }
