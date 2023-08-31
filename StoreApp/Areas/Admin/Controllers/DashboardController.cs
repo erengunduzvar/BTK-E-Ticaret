1f056bcc62fc38d011aba05c6b97c9baa8da2fca
Eren Gündüzvar <70322002+erengunduzvar@users.noreply.github.com>
2023-08-31 18:11:55 +0300

    Manage Orders

--- StoreApp/Areas/Admin/Controllers/DashboardController.cs
+++ StoreApp/Areas/Admin/Controllers/DashboardController.cs
@@ -4,7 +4,7 @@ namespace StoreApp.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class DashboardController : Controller
-    {[Area("Admin")]
+    {
         public IActionResult Index()
         {
             return View();
