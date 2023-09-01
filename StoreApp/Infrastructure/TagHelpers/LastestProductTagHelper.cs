652393dbed492b0304dba79397e84294910b372c
Eren Gündüzvar <70322002+erengunduzvar@users.noreply.github.com>
2023-09-01 23:19:22 +0300

    Tag Helpers Html Attribute Name

--- StoreApp/Infrastructure/TagHelpers/LastestProductTagHelper.cs
+++ StoreApp/Infrastructure/TagHelpers/LastestProductTagHelper.cs
@@ -9,6 +9,8 @@ namespace StoreApp.Infrastructure.TagHelpers
     public class LastestProductTagHelper : TagHelper
     {
         private readonly IServiceManager _manager;
+        [HtmlAttributeName("number")]
+        public int Number { get; set; }
 
         public LastestProductTagHelper(IServiceManager manager)
         {
@@ -31,7 +33,7 @@ namespace StoreApp.Infrastructure.TagHelpers
 
             TagBuilder ul = new TagBuilder("ul");
 
-            var products = _manager.ProductService.GetLatestProducts(5,false);
+            var products = _manager.ProductService.GetLatestProducts(Number,false);
             foreach (var product in products)
             {
                 TagBuilder li = new TagBuilder("li");
